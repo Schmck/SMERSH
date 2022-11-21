b42db9bfbefac043d715a0347c109a5273cd2a41
Schmck <rsk.rook@gmail.com>
2022-11-21 09:59:42 +0100

    fixes

--- Web/Controllers/Current/CurrentChatController.ts
+++ Web/Controllers/Current/CurrentChatController.ts
@@ -8,17 +8,13 @@ import * as Main from "dotenv/lib/main";
 export class CurrentChatController {
     @Get(ChatRoute.GetChat.Action)
     public getCurrentChat() {
-        //Main.config()
-        const config = process.env;
-        //const session = new WebAdminSession(config["BASE_URL"], config["AUTHCRED"])
-        //const session = WebAdminSession.set(config["BASE_URL"], config["AUTHCRED"])
-        let session = WebAdminSession.get();
+        const session = WebAdminSession.get();
 
         const result = session.navigate(ChatRoute.GetChat.Action)
-        return result.then(page => {
+        return result.then(dom => {
             const messages = []
-            if (page) {
-                page.window.document.querySelectorAll(".chatmessage").forEach(msg => {
+            if (dom) {
+                dom.window.document.querySelectorAll(".chatmessage").forEach(msg => {
                     let username = msg.querySelector('.username').innerHTML
                     let message = msg.querySelector('.message').innerHTML
                     let visibility = msg.querySelector('.teamnotice').innerHTML
