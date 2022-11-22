45d9c5953854e93cfdcd9cb0d8aff3b7743ef6d6
Schmck <rsk.rook@gmail.com>
2022-11-22 22:46:24 +0100

    working on chat controller

--- Web/Controllers/Current/CurrentChatController.ts
+++ Web/Controllers/Current/CurrentChatController.ts
@@ -1,11 +1,16 @@
 ﻿import { Controller, Param, Body, Get, Post, Put, Delete } from 'routing-controllers';
 import { WebAdminSession } from '../../../Services/WebAdmin';
 import { ChatRoute } from '../../../Services/WebAdmin/Routes';
+import { Logger, dummyLogger, Logger as ILogger } from "ts-log";
 import { Config } from '../../Framework'
 import * as Main from "dotenv/lib/main";
 
 @Controller()
 export class CurrentChatController {
+    public constructor(private readonly log: Logger = dummyLogger) { }
+
+
+
     @Get(ChatRoute.GetChat.Action)
     public getCurrentChat() {
         const session = WebAdminSession.get();
@@ -13,20 +18,41 @@ export class CurrentChatController {
         const result = session.navigate(ChatRoute.GetChat.Action)
         return result.then(dom => {
             const messages = []
+            this.log.info(dom)
+            this.log.info(dom.window)
+            this.log.info(dom.window.document)
             if (dom) {
+                this.log.info(dom)
+                this.log.info(dom.window)
+                this.log.info(dom.window.document)
                 dom.window.document.querySelectorAll(".chatmessage").forEach(msg => {
-                    let username = msg.querySelector('.username').innerHTML
-                    let message = msg.querySelector('.message').innerHTML
-                    let visibility = msg.querySelector('.teamnotice').innerHTML
-                    messages.push({
+                    let username
+                    let message
+                    let visibility
+
+                    if (msg.querySelector('.username')) {
+                        username = msg.querySelector('.username').innerHTML
+                    }
+
+                    if (msg.querySelector('.message')) {
+                        message = msg.querySelector('.message').innerHTML
+                    }
+
+                    if (msg.querySelector('.teamnotice')) {
+                        visibility = msg.querySelector('.teamnotice').innerHTML
+                    }
+                    const usermsg = {
                         username,
                         message,
                         visibility
-                    })
+                    }
+                    console.log(usermsg)
+                    this.log.info(usermsg)
+                    messages.push(usermsg)
                 })
             }
 
-            return messages
+            return dom.window.document.querySelectorAll(".chatmessage")
         })
     }
 }
