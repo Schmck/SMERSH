466a4438a12d6f4f448cf7fb21d1b65e3ba0925c
Schmock <rsk.rook@gmail.com>
2023-04-10 21:59:06 +0200

    PlayerNameChangedEvent includes old name

--- Watcher/Round/RoundWatcher.ts
+++ Watcher/Round/RoundWatcher.ts
@@ -3,7 +3,7 @@ import { WebAdminSession } from '../../Services/WebAdmin';
 import { ChatRoute } from '../../Services/WebAdmin/Routes';
 import { StartRoundCommand, EndRoundCommand, ChangeMapCommand  } from '../../Commands/Round'
 import { UpdatePlayerRoundCommand  } from '../../Commands/Round/PlayerRound'
-import { RegisterPlayerCommand  } from '../../Commands/Player'
+import { RegisterPlayerCommand, ChangePlayerNameCommand  } from '../../Commands/Player'
 import { StatusQuery } from '../../Services/WebAdmin/Queries'
 import { Guid } from 'guid-typescript'
 import { SearchClient } from '../../Elastic'
@@ -89,7 +89,7 @@ export class RoundWatcher extends Watcher {
 
                 if (playerIds.length) {
                     for (let playerId of playerIds) {
-                        const exists = await SearchClient.Exists(playerId, PlayerSearchReport)
+                        const exists = await SearchClient.Get(playerId as any as Guid, PlayerSearchReport)
                         const player = status.players.find(player => player.Id === playerId)
                         
 
@@ -109,7 +109,11 @@ export class RoundWatcher extends Watcher {
 
 
                             }
-                        } else if (round && player && player.Id && newMapTime && newMapTime === mapTime && mapTime !== prevMapTime) {
+                        } else if (player && exists.Name !== player.Playername) {
+                            await this.commandBus.execute(new ChangePlayerNameCommand(player.Id, player.Playername))
+                        }
+
+                        if (exists && round && player && player.Id && newMapTime && newMapTime === mapTime && mapTime !== prevMapTime) {
                             const team = Team.fromValue(parseInt(player.Team))
                             const role = Role.fromDisplayName(player.Role);
                             const id = Guid.parse((round.Id.toString().slice(0, 27) + playerId.slice(9)))
