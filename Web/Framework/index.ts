58b603bf9b0c1c015bdf5e14e108faecfccc23eb
Schmock <rsk.rook@gmail.com>
2023-04-16 18:20:54 +0000

    fixes

--- Web/Framework/index.ts
+++ Web/Framework/index.ts
@@ -4,4 +4,3 @@ export { ClientBuilder } from './ClientBuilder';
 export { Api } from './Api';
 export { SmershModule } from './smersh.module'
 export { AppModule } from './app.module'
-export { WebAdminModule } from './webadmin.module'
