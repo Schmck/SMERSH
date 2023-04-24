48c3ca66f68aff8e13f1043f035034a68f10ca9c
Schmock <rsk.rook@gmail.com>
2023-04-24 23:06:45 +0200

    actual fix

--- SMERSH/ValueObjects/Enumeration.ts
+++ SMERSH/ValueObjects/Enumeration.ts
@@ -9,15 +9,15 @@
 
 
     public static getAll<T extends Enumeration>(): T[] {
-        const enums: T[] = Object.getOwnPropertyNames(this).slice(3).map(role => this[role])
+        const enums: T[] = Object.values(this).filter(obj => typeof(obj) !== "function")
         return enums
     }
 
     public static fromValue<T extends Enumeration>(value: number) : T {
-        return this.getAll<T>().find(obj => obj.Value === value)
+        return this.getAll<T>().filter((obj : T) => obj.Value === value)[0]
     }
 
     public static fromDisplayName<T extends Enumeration>(displayName: string) : T {
-        return this.getAll<T>().find(obj => obj.DisplayName === displayName)
+        return this.getAll<T>().filter((obj: T) => obj.DisplayName === displayName)[0]
     }
 }
