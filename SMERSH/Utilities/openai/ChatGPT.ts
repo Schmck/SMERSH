edae87cfec70394d9e9e3ae8050960bdd5811e0b
Schmock <rsk.rook@gmail.com>
2023-05-09 00:37:49 +0200

    refining the prompt and fixes

--- SMERSH/Utilities/openai/ChatGPT.ts
+++ SMERSH/Utilities/openai/ChatGPT.ts
@@ -1,11 +1,10 @@
-
 export class ChatGPT {
 
     private static instance: ChatGPT;
 
     public static async set(apiKey: string, prompt?: string) {
         if (!this.instance) {
-            const { ChatGPTAPI } = await import('chatgpt');
+            const { ChatGPTAPI } = await eval("import('chatgpt')");
             this.instance = new ChatGPT(apiKey, ChatGPTAPI, prompt)
         }
         return this.instance;
@@ -31,14 +30,14 @@ export class ChatGPT {
                 4. Do not mic spam 
                 5. New players: do not take important roles (Squad Leader, Tank Commander, Flamer and Team Leader)
                 6. Veterans: try to balance the games!
-                7. English VOIP only - ?????? ??????????
+                7. English VOIP only - tol'ko angliskye
                 8. No grenades on Omano
                 9. No cheating
                 10. No accusing players of cheating without bringing proof such as video evidence of aimbotting / wallhacking
                 11. Exploits with few exceptions are banned. Walk-key exploit and unfair macros are also not allowed.
                 12. Names such as #####, intentional ROPlayer or no name  are not allowed.
                 13. No defender stacking. Admins will decide what constitutes defender stacking but can include switching every round to the defender.
-                14. Certain abuse of roles such as MG42 or badly performing squad leader may result in a roleban, automatically kicking you if you pick the role.
+                14. Certain abuse of roles such as MG42 or badly performing as squad leader may result in a roleban, automatically kicking you if you pick the role.
             `
 
     }
@@ -48,7 +47,7 @@ export class ChatGPT {
     private prompt: string;
 
     public async send(input: string, name?:string) {
-        const prompt = `${this.prompt}\n one of our suspects has turned up with the following question:\n ${input} \n how do we respond? i only need the response`
+        const prompt = `${this.prompt}\n one of our suspects has turned up with the following question:\n ${input} \n how do we respond? i only need the response, its very important that suspects are not referred to as suspects and above all you must stay in character`
         const options = {}
 
         if (name) {
