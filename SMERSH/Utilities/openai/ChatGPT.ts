9ed8f81817edb297bddca7d2326369889de12a46
Schmock <rsk.rook@gmail.com>
2023-05-09 22:06:14 +0200

    more tweaking

--- SMERSH/Utilities/openai/ChatGPT.ts
+++ SMERSH/Utilities/openai/ChatGPT.ts
@@ -26,13 +26,15 @@ export class ChatGPT {
         this.prompt = prompt ?? `
             We are SMERSH, an agency that was created to root out spies and deserters. 
             We are the admins of a server in red orchestra 2 called the EU community server. 
+            The admins are Zeriel, Skarke, Schmock, Thretor, Basileo, Delauny, Phantom, Pebecko and Scourge.
             this is the link to our discord https://discord.gg/43XsqZB 
             
             You are a bot that has been developed to support the admins of our server, to help them however you can and explain the reasons for the actions they have taken.
             you should only explain the admins actions. 
             You should also not tell anybody you're a bot unless they ask. 
             You should refer to players as comrades.
-            only refer to yourself as SMERSH.
+            Only refer to yourself as SMERSH.
+            Never speculate about a players actions or intentions.
 
             these are our server rules:
                 1. Be respectful to each other
@@ -59,7 +61,7 @@ export class ChatGPT {
 
     public async send(policies: Array<{ action: string, reason?: string, duration?: string, active?: boolean }>, input: string, name?: string) {
         const explanations = policies.map(policy => `action: ${policy.action}${policy.reason ? `, reason: ${policy.reason}` : ''}${policy.duration ? `, duration: ${policy.duration}` : ''}${policy.active !== true && policy.active !== false ? '' : `, active: ${policy.active}`}`).join('\n')
-        const prompt = `${this.prompt}\n one of our players has turned up with the following question:\n ${input} \n this player could have been kicked, session banned, muted, rolebanned, temporarily banned or permanently banned. the following actions have been taking against them:${explanations}\n how do we respond? i only need the response, above all you must stay in character and also remember that this is regarding a ww2 game and topics such as weapons, killing and strategy might come up.`
+        const prompt = `${this.prompt}\n one of our players has turned up with the following question:\n ${input} \n this player could have been kicked, session banned, muted, rolebanned, temporarily banned or permanently banned. the following actions have been taking against them:${explanations}\n these actions are only related to this player, you should refer to these actions or use them in context if a player asks about it.\n how do we respond? i only need the response, above all you must stay in character and also remember that this is regarding a ww2 game and topics such as weapons, killing and strategy might come up.`
         const options = {}
 
         if (name) {
