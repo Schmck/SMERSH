9c8c4fa77d35a1f95e022cb93259935f2241d15c
Schmock <rsk.rook@gmail.com>
2023-05-14 14:44:30 +0200

    forgot choices

--- Discord/Commands/LayoutCommand.ts
+++ Discord/Commands/LayoutCommand.ts
@@ -51,14 +51,15 @@ export const LayoutCommand: Command = {
         },
         {
             name: 'start_time',
-            description: 'minimum amount of players needed for the layout to be active',
-            type: ApplicationCommandOptionType.String,
-            choices: [{ "name": "12am", "value": "0" }, { "name": "1am", "value": "1" }, { "name": "2am", "value": "2" }, { "name": "3am", "value": "3" }, { "name": "4am", "value": "4" }, { "name": "5am", "value": "5" }, { "name": "6am", "value": "6" }, { "name": "7am", "value": "7" }, { "name": "8am", "value": "8" }, { "name": "9am", "value": "9" }, { "name": "10am", "value": "10" }, { "name": "11am", "value": "11" }, { "name": "12pm", "value": "12" }, { "name": "1pm", "value": "13" }, { "name": "2pm", "value": "14" }, { "name": "3pm", "value": "15" }, { "name": "4pm", "value": "16" }, { "name": "5pm", "value": "17" }, { "name": "6pm", "value": "18" }, { "name": "7pm", "value": "19" }, { "name": "8pm", "value": "20" }, { "name": "9pm", "value": "21" }, { "name": "10pm", "value": "22" }, { "name": "11pm", "value": "23" }]
+            description: 'when this layout should start being enforced',
+            choices: [{ "name": "12am", "value": "0" }, { "name": "1am", "value": "1" }, { "name": "2am", "value": "2" }, { "name": "3am", "value": "3" }, { "name": "4am", "value": "4" }, { "name": "5am", "value": "5" }, { "name": "6am", "value": "6" }, { "name": "7am", "value": "7" }, { "name": "8am", "value": "8" }, { "name": "9am", "value": "9" }, { "name": "10am", "value": "10" }, { "name": "11am", "value": "11" }, { "name": "12pm", "value": "12" }, { "name": "1pm", "value": "13" }, { "name": "2pm", "value": "14" }, { "name": "3pm", "value": "15" }, { "name": "4pm", "value": "16" }, { "name": "5pm", "value": "17" }, { "name": "6pm", "value": "18" }, { "name": "7pm", "value": "19" }, { "name": "8pm", "value": "20" }, { "name": "9pm", "value": "21" }, { "name": "10pm", "value": "22" }, { "name": "11pm", "value": "23" }],
+            type: ApplicationCommandOptionType.String
         },
         {
             name: 'end_time',
-            description: 'minimum amount of players needed for the layout to be active',
-            type: ApplicationCommandOptionType.String,
+            description: 'when this layout should stop being enforced',
+            choices: [{ "name": "12am", "value": "0" }, { "name": "1am", "value": "1" }, { "name": "2am", "value": "2" }, { "name": "3am", "value": "3" }, { "name": "4am", "value": "4" }, { "name": "5am", "value": "5" }, { "name": "6am", "value": "6" }, { "name": "7am", "value": "7" }, { "name": "8am", "value": "8" }, { "name": "9am", "value": "9" }, { "name": "10am", "value": "10" }, { "name": "11am", "value": "11" }, { "name": "12pm", "value": "12" }, { "name": "1pm", "value": "13" }, { "name": "2pm", "value": "14" }, { "name": "3pm", "value": "15" }, { "name": "4pm", "value": "16" }, { "name": "5pm", "value": "17" }, { "name": "6pm", "value": "18" }, { "name": "7pm", "value": "19" }, { "name": "8pm", "value": "20" }, { "name": "9pm", "value": "21" }, { "name": "10pm", "value": "22" }, { "name": "11pm", "value": "23" }],
+            type: ApplicationCommandOptionType.String
         },
     ],
     autocomplete: async (client: Client, interaction: AutocompleteInteraction): Promise<void> => {
