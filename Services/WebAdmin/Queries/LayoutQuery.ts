26bac6f053edb7edcfe2659ea72990c8f77df46a
Schmock <rsk.rook@gmail.com>
2023-08-27 16:48:08 +0200

    layout save fixes

--- Services/WebAdmin/Queries/LayoutQuery.ts
+++ Services/WebAdmin/Queries/LayoutQuery.ts
@@ -8,15 +8,27 @@ export class LayoutQuery extends Query {
     public static async Get() {
         const session = WebAdminSession.get();
         const dom = await session.navigate(LayoutRoute.GetLayout.Action)
+        const env = JSON.parse(process.argv[process.argv.length - 1]);
 
-        const campaign = Object.values(dom.window.document.querySelectorAll(`[id^='tcontainer_sg']`))
+        const suffix = env['GAME'] && env['GAME'] === 'RS1' ? 'pt' : 'sg'
+        const campaign = Object.values(dom.window.document.querySelectorAll(`[id^='tcontainer_${suffix}']`))
+        if (!campaign.length) {
+            return null
+        }
         const layout: Record<string, string[]> = Object.fromEntries(Array.from(campaign).map(item => {
             let key = item.querySelector('legend').innerText
-            let value = item.querySelector('textarea').innerText.split('\n')
+            let values = item.querySelector('textarea');
+            if (!values.innerText) {
+                return [key, []]
+            }
+            let value = values && values.innerText && values.innerText.split('\n')
 
             return [key, value]
         }, {}))
 
+        if (!Object.values(layout).every(lt => lt && lt.length)) {
+            return null;
+        }
         return layout
     }
 }
